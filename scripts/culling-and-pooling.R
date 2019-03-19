88b5ee56837df8c3024af4acf87c200e3d208676
teo <teofiln@gmail.com>
2019-03-19 14:51:39 -0500

    script to keep track of changes made during the experiment and to update the metadata files accordingly

---
+++ scripts/culling-and-pooling.R
@@ -0,0 +1,92 @@
+# weed out the bad growers and combine strains from 2 zones into 1 plate
+# groups will be by replicate, so 4 strains from zone A rep 1 go with zone B rep 1
+# combinations: A+B, F+I, J+K, P+D
+# for subsalsum (S) take the best 4 and split into 2 reps within the same plate
+
+#### INOCULATION
+# Do the first inoculation BY HAND by transferring 800 of culture into 200 of media
+# This should provide enough cover for 1-2 days of growth before a within-app transfer can be made
+####
+
+# helper functions
+make_plate <-
+  function(plate_name,
+           plate_number,
+           treatments,
+           strains) {
+    wells <-
+      expand.grid(Row = LETTERS[1:6], Column = 1:8) %>% mutate(Well = paste0(Row, Column))
+    trts  <- rep(treatments, 8)
+    strs  <- rep(strains, each = 6)
+    filled_plate <-
+      tibble(
+        plate_name,
+        plate_number,
+        row = wells$Row,
+        column = wells$Column,
+        well = wells$Well,
+        treatment = trts,
+        strain = strs,
+        replicate = str_extract(plate_name, regex("\\d{1}"))
+      )
+    return(filled_plate)
+  }
+
+# format so that a new plate metadata can be appended to all_plates_meta directly
+format_for_append <- . %>%
+  mutate(experiment = "8x6") %>%
+  mutate(zone = str_extract(strain, regex("^[:alpha:]{1}"))) %>%
+  select(experiment,
+         plate_number,
+         plate_name,
+         zone,
+         strain,
+         treatment,
+         row:well)
+
+# 2019-03-19
+# combine F and I
+F_keepers <- paste("F.", c("1.2a",  "1.5b", "1.30a", "1.37a"), sep="")
+I_keepers <- paste("I.", c("3.11a", "3.22b", "3.28a", "5.3a"), sep="")
+FI_keepers <- c(F_keepers, I_keepers)
+
+FI1_plate <- make_plate(
+  plate_name = "fi1",
+  plate_number = 30,
+  treatments = c(8, 12, 16, 20, 24, 28),
+  strains = FI_keepers
+)
+
+FI1_plate <- FI_plate %>% format_for_append
+
+FI2_plate <- make_plate(
+  plate_name = "fi1",
+  plate_number = 30,
+  treatments = c(8, 12, 16, 20, 24, 28),
+  strains = FI_keepers
+)
+
+FI2_plate <- FI_plate %>% format_for_append
+
+FI3_plate <- make_plate(
+  plate_name = "fi1",
+  plate_number = 30,
+  treatments = c(8, 12, 16, 20, 24, 28),
+  strains = FI_keepers
+)
+
+FI3_plate <- FI_plate %>% format_for_append
+
+##### IMPORTANT ######
+# When a combined plate is made, append its metadata to the main all_plates_meta.csv/Rsave file
+# this will overwrite the main metadata file, so run only once per plate
+# if the meta for a particular new plate has changed, remove the old plate and append the new
+if (FALSE) {
+  all_plates_meta <- read_csv("/data/Dropbox/Skeletonema-marinoi-salinity-reaction-norms-data/metadata/all_plates_meta.csv")
+  all_plates_meta <- bind_rows(all_plates_meta, FI1_plate, FI2_plate, FI3_plate)
+  write_csv(all_plates_meta, path = "/data/Dropbox/Skeletonema-marinoi-salinity-reaction-norms-data/metadata/all_plates_meta.csv")
+  save(all_plates_meta, file = "/data/Dropbox/Skeletonema-marinoi-salinity-reaction-norms-data/metadata/all_plates_meta.csv")
+}
+
+#####
+
